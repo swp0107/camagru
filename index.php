7ee2e5d350eeab1f03bb48ac6e7ae27b381c173e
Thomas Kim <thomkim@e1z2r9p9.42.us.org>
2019-05-07 16:41:43 -0700

    main page

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Camagru Login and Signup</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<style>
+    body{
+        overflow-x: hidden;
+    }
+    #centered1{
+        position: absolute;
+        font-size: 10vw;
+        top: 30%;
+        left: 30%;
+        transform: translate(-50%, -50%);
+    }
+    #centered2{
+        position: absolute;
+        font-size: 10vw;
+        top: 50%;
+        left: 40%;
+        transform: translate(-50%, -50%);
+    }
+    #centered3{
+        position: absolute;
+        font-size: 10vw;
+        top: 70%;
+        left: 30%;
+        transform: translate(-50%, -50%);
+    }
+</style>
+<body>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="well">
+                <center><h1>camagru</h1></center>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6" style="Left:0.5%;">
+            <img src="images/sky.png" class="img-rounded" title="Camagru - Social Networking" width="650px" height="560px">
+            <div id="centered1" class="centered"><h3 style="color:white;"><span class="glyphicon glyphicon-search"></span>&nbsp&nbsp<strong>Follow Your Interests.</strong></h3></div>
+            <div id="centered2" class="centered"><h3 style="color:white;"><span class="glyphicon glyphicon-search"></span>&nbsp&nbsp<strong>Hear what People are talking about.</strong></h3></div>
+            <div id="centered3" class="centered"><h3 style="color:white;"><span class="glyphicon glyphicon-search"></span>&nbsp&nbsp<strong>Join the Community.</strong></h3></div>
+        </div>
+        <div class="col-sm-6" style="left:8%">
+            <img src="images/camagru.png" class="img-rounded" title="Camagru - Social Networking" width="80px" height="80px">
+            <h2><strong>See what's happening in <br> the world right now</strong></h2><br><br>
+            <h4><strong>Join Camagru Today.</strong></h4>
+        </div>
+   </div>
+</body>
+</html>
